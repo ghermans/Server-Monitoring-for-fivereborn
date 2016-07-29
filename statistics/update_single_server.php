cd96e322a31ee330b01705bd80d2523844d78b9f
xyShadowyx <xyShadowyx@users.noreply.github.com>
2016-07-29 17:55:30 +0200

    Update update_single_server.php

--- statistics/update_single_server.php
+++ statistics/update_single_server.php
@@ -5,11 +5,7 @@ if(sizeof($argv) == 2) {
 	$ipAddress = $argv[1];
 	$res = preg_match('/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,5}$/', $ipAddress, $ip_matches);
 	if ($res) {
-
 		$api = new API();
-		
-		Log::warning("checkUpdateSingleServer File called!");
-		
 		$api->checkUpdateSingleServer($ipAddress);
 	}
 }
